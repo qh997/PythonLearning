3031bec85db7cc850e5e8afba27dc07a8277c51a
Geng Shuang <gengs@neusoft.com>
2012-08-13 16:00:31 +0800

    《Python基础教程》 第一章 基础知识

---
+++ BeginningPython/Chapter01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+print "Hello, world!"
+
+name = raw_input("What is your name? ")
+print "Hello, " + name + "!"
+
+print '''This is a very long string.
+It continues here.
+And it's note over yet.
+"Hello, world!"
+Still here.'''
+
+path = r'C:\nowhere'
+print path
